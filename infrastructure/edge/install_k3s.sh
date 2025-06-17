80059f3836c68b2f7dfe8dc1bd129081baa9ad5c
nguu0123 <nguu0123@gmail.com>
2025-06-17 16:39:26 +0300

    Add k3s setup and Jaeger route
    - Introduce `install_k3s.sh` for deploying k3s on edge with custom configurations.
    - Add `observability_gateway.yml` for configuring HTTPRoute in observability stack.

---
+++ infrastructure/edge/install_k3s.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install k3s master
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--flannel-backend=none --disable-network-policy --write-kubeconfig-mode "0644" --disable servicelb,traefik' sh -
+
+# Copy kubeconfig and set permission
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config && chown "$USER" ~/.kube/config && chmod 600 ~/.kube/config && export KUBECONFIG=~/.kube/config
