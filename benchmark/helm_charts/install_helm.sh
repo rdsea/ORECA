566458d75715447e705c81f76df86de432c9b485
nguu0123 <nguu0123@gmail.com>
2025-08-01 12:29:49 +0300

    Update Helm installations and remove OTEL files
    - Add Helm charts for Cilium, Prometheus, Longhorn, Jaeger,
      and OpenTelemetry Collector.
    - Remove outdated OTEL collector deployment and config files.
    - Update local Prometheus URL in monitor config for coherence.

--- benchmark/helm_charts/install_helm.sh
+++ benchmark/helm_charts/install_helm.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+# Cilium
+helm repo add cilium https://helm.cilium.io/
 helm install cilium cilium/cilium --version 1.17.5 \
   --namespace kube-system --values cilium_values.yaml
+
+# Prometheus
+# Create namespace for grafana dashboard as this's not automatically done by helm
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl create namespace dashboard
+helm install prometheus prometheus-community/kube-prometheus-stack -n observe --values values.yaml --create-namespace
+
+# Longhorn
+# Install requirement before deploying longhorn to all node https://longhorn.io/docs/1.9.0/deploy/install/#installing-open-iscsi
+helm repo add longhorn https://charts.longhorn.io
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.9.0 --values values.yaml
+
+# Jaeger
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm install jaeger jaegertracing/jaeger -n observe --create-namespace -f values.yaml
+
+# Otel collector
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm install my-opentelemetry-collector open-telemetry/opentelemetry-collector \
+  -f values.yaml -n observe
