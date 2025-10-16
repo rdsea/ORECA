d8e9c616bd919b2b9ede4d2c59425ed70efb9234
nguu0123 <nguu0123@gmail.com>
2025-10-16 15:49:01 +0300

    Add code for train-ticket

---
+++ benchmark/experiment_controller/docs/train_ticket/experiment.py
@@ -0,0 +1,103 @@
+import pathlib
+
+import yaml
+from experiment_controller.config.experiment_config import (
+    RCAExperimentConfig,
+)
+from experiment_controller.experiment_controller import RCAExperiment
+from experiment_controller.logger import logger
+from experiment_controller.rca_evaluator import RCAEvaluator
+from rca_methods.rca_factory import RCAMethodEnum
+
+SERVICE = [
+    "ts-admin-basic-info-service",
+    "ts-admin-order-service",
+    "ts-admin-route-service",
+    "ts-admin-travel-service",
+    "ts-admin-user-service",
+    "ts-assurance-service",
+    "ts-auth-service",
+    "ts-avatar-service",
+    "ts-basic-service",
+    "ts-cancel-service",
+    "ts-common",
+    "ts-config-service",
+    "ts-consign-price-service",
+    "ts-consign-service",
+    "ts-contacts-service",
+    "ts-delivery-service",
+    "ts-execute-service",
+    "ts-food-delivery-service",
+    "ts-food-service",
+    "ts-gateway-service",
+    "ts-inside-payment-service",
+    "ts-news-service",
+    "ts-notification-service",
+    "ts-order-other-service",
+    "ts-order-service",
+    "ts-payment-service",
+    "ts-preserve-other-service",
+    "ts-preserve-service",
+    "ts-price-service",
+    "ts-rebook-service",
+    "ts-route-plan-service",
+    "ts-route-service",
+    "ts-seat-service",
+    "ts-security-service",
+    "ts-station-food-service",
+    "ts-station-service",
+    "ts-ticket-office-service",
+    "ts-train-food-service",
+    "ts-train-service",
+    "ts-travel2-service",
+    "ts-travel-plan-service",
+    "ts-travel-service",
+    "ts-ui-dashboard",
+    "ts-user-service",
+    "ts-verification-code-service",
+    "ts-voucher-service",
+    "ts-wait-order-service",
+]
+FAULT = [
+    "network-delay",
+    "resource-cpu",
+    "resource-memory",
+]
+
+if __name__ == "__main__":
+    current_path = pathlib.Path(__file__).parent
+    for service in SERVICE:
+        for fault in FAULT:
+            config_path = (
+                current_path / "config.local" / f"{service}-{fault}" / "config.yaml"
+            )
+            with open(config_path) as f:
+                config_data = yaml.safe_load(f)
+
+            experiment_config = RCAExperimentConfig.model_validate(config_data)
+            experiment = RCAExperiment(
+                experiment_config,
+                pathlib.Path(__file__).parent / "train_ticket_result.local",
+            )
+            logger.info(f"Starting experiment: {experiment_config.experiment_name}")
+            # Uncomment to run
+            # experiment.run()
+            logger.info(f"Finished experiment: {experiment_config.experiment_name}")
+
+    current_path = pathlib.Path(__file__).parent
+
+    RCA_METHODS_TO_EVALUATE = [
+        RCAMethodEnum.BARO,
+        RCAMethodEnum.DUMMY,
+        # RCAMethodEnum.CAUSALAI,
+        # RCAMethodEnum.CAUSALRCA,
+        # RCAMethodEnum.CIRCA,
+        # RCAMethodEnum.CLOUDRANGER,
+    ]
+
+    rca_evaluator = RCAEvaluator(
+        current_path / "train_ticket_result.local",
+        RCA_METHODS_TO_EVALUATE,
+    )
+
+    # rca_evaluator.create_report()
---
+++ benchmark/experiment_controller/docs/train_ticket/generate_config.py
@@ -0,0 +1,193 @@
+import os
+import pathlib
+
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader("."),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+template = env.get_template("experiment_config_template.yaml.j2")
+
+data = {
+    # "experiment_name": "network_delay_ensemble",
+    "number_of_run": 1,
+    "time_between_run": "60s",
+    "clean_up": {
+        "activate": False,
+        "observability_cleanup_script": "/u/49/anhdun1/unix/git/RCA_Edge_Cloud/benchmark/experiment_controller/docs/train_ticket/scripts/application_cleanup_script.sh",
+        "application_cleanup_script": "/u/49/anhdun1/unix/git/RCA_Edge_Cloud/benchmark/experiment_controller/docs/train_ticket/scripts/observability_cleanup_script.sh",
+    },
+    # "fault_config": {
+    #     "name": "network-delay-ensemble",
+    #     "duration": "300s",
+    #     "fault_type": "NetworkFault.DELAY",
+    #     "target": {
+    #         "namespace": "default",
+    #         "environment": ["edge"],
+    #         "label_selectors": {"app": "ensemble"},
+    #     },
+    #     "fault_specific_config": {
+    #         "namespace": "default",
+    #         "duration": "300s",
+    #         "delay": {"latency": "50ms", "correlation": "0.5", "jitter": "20ms"},
+    #     },
+    # },
+    # "ground_truth": "ensemble_service:rtt",
+    "anomaly_injection_period": "900s",
+    "warm_up_interval": "300s",
+    "workload": {
+        "type": "docker",
+        "config": {
+            "image": "rdsea/object_detection_client:latest",
+            "args": {
+                "host": "http://XXX.XXX.XXX.XXX:30467",
+                "user": "50",
+                "run-time": "1200s",
+                "spawn-rate": "0.1",
+                "f": "main.py",
+            },
+        },
+        "list_of_generator": ["edge-raspi3.cs.aalto.fi"],
+        "ssh_username": "aaltosea",
+    },
+    "data_collector_config": {
+        "metric_url": "http://XXX.XXX.XXX.XXX/prometheus",
+        "trace_url": "http://XXX.XXX.XXX.XXX/tempo",
+    },
+    "observability_cadence_config": {
+        "metric_config": {
+            "environment": ["edge", "cloud"],
+            "scrape_interval": "1s",
+            "evaluation_interval": "1s",
+        }
+    },
+}
+
+SERVICE = [
+    "ts-admin-basic-info-service",
+    "ts-admin-order-service",
+    "ts-admin-route-service",
+    "ts-admin-travel-service",
+    "ts-admin-user-service",
+    "ts-assurance-service",
+    "ts-auth-service",
+    "ts-avatar-service",
+    "ts-basic-service",
+    "ts-cancel-service",
+    "ts-common",
+    "ts-config-service",
+    "ts-consign-price-service",
+    "ts-consign-service",
+    "ts-contacts-service",
+    "ts-delivery-service",
+    "ts-execute-service",
+    "ts-food-delivery-service",
+    "ts-food-service",
+    "ts-gateway-service",
+    "ts-inside-payment-service",
+    "ts-news-service",
+    "ts-notification-service",
+    "ts-order-other-service",
+    "ts-order-service",
+    "ts-payment-service",
+    "ts-preserve-other-service",
+    "ts-preserve-service",
+    "ts-price-service",
+    "ts-rebook-service",
+    "ts-route-plan-service",
+    "ts-route-service",
+    "ts-seat-service",
+    "ts-security-service",
+    "ts-station-food-service",
+    "ts-station-service",
+    "ts-ticket-office-service",
+    "ts-train-food-service",
+    "ts-train-service",
+    "ts-travel2-service",
+    "ts-travel-plan-service",
+    "ts-travel-service",
+    "ts-ui-dashboard",
+    "ts-user-service",
+    "ts-verification-code-service",
+    "ts-voucher-service",
+    "ts-wait-order-service",
+]
+FAULT = [
+    "network-delay",
+    "resource-cpu",
+    "resource-memory",
+]
+FAULT_ROOT_CAUSE = {
+    "network-delay": "service:rtt",
+    "resource-cpu": "service:cpu_usage",
+    "resource-memory": "service:memory_usage",
+}
+FAULT_CONFIG = {
+    "network-delay": {
+        # "name": "network-delay-ensemble",
+        "duration": "300s",
+        "fault_type": "NetworkFault.DELAY",
+        "target": {
+            "namespace": "default",
+            "environment": ["cloud1"],
+            # "label_selectors": {"app": "ensemble"},
+        },
+        "fault_specific_config": {
+            "namespace": "default",
+            "duration": "300s",
+            "delay": {"latency": "50ms", "correlation": "0.5", "jitter": "20ms"},
+        },
+    },
+    "resource-cpu": {
+        # "name": "resource-cpu-ensemble",
+        "duration": "300s",
+        "fault_type": "ResourceHog.CPU",
+        "target": {
+            "namespace": "default",
+            "environment": ["cloud1"],
+            # "label_selectors": {"app": "ensemble"},
+        },
+        "fault_specific_config": {
+            "namespace": "default",
+            "duration": "300s",
+            "stress_cpu": {"workers": 1, "load": 100},
+        },
+    },
+    "resource-memory": {
+        # "name": "resource-memory-ensemble",
+        "duration": "300s",
+        "fault_type": "ResourceHog.MEMORY",
+        "target": {
+            "namespace": "default",
+            "environment": ["cloud1"],
+            # "label_selectors": {"app": "ensemble"},
+        },
+        "fault_specific_config": {
+            "namespace": "default",
+            "duration": "300s",
+            "stress_memory": {"workers": 1, "size": "150MB"},
+        },
+    },
+}
+current_path = pathlib.Path(__file__).parent
+for service in SERVICE:
+    for fault in FAULT:
+        experiment_name = f"{fault}_{service}"
+        fault_config = FAULT_CONFIG[fault]
+        fault_config["name"] = f"{fault}_{service}"
+        fault_config["ground_truth"] = f"{service}_service:rtt"
+        fault_config["target"]["label_selectors"] = {"app": service}
+        rendered_yaml = template.render(
+            {
+                **data,
+                "fault_config": fault_config,
+                "experiment_name": f"{fault}_{service}",
+                "ground_truth": f"{service}_{FAULT_ROOT_CAUSE[fault]}",
+            }
+        )
+        experiment_path = current_path / "config.local" / f"{service}-{fault}"
+        os.makedirs(experiment_path, exist_ok=True)
+        with open(experiment_path / "config.yaml", "w") as f:
+            f.write(rendered_yaml)
