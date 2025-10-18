74d09c4b2876940cb69ef63d11caa28f0d66d7ba
nguu0123 <nguu0123@gmail.com>
2025-10-18 23:41:39 +0300

    Add train ticket application cleanup script

---
+++ benchmark/experiment_controller/docs/train_ticket/scripts/application_cleanup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TRAIN_TICKET_SOURCE="$SCRIPT_DIR/../train-ticket-source"
+#
+# Check if the directory is empty
+if [ -z "$(ls -A "$TRAIN_TICKET_SOURCE")" ]; then
+  echo "Error: Directory '$TRAIN_TICKET_SOURCE' is empty.  Run git submodule update --init"
+  exit 1
+fi
+
+cd "$TRAIN_TICKET_SOURCE" || exit 1
+make reset-deploy Namespace=train-ticket
+kubectl delete ns train-ticket
+
+sleep 5
+kubectl create ns train-ticket
+make deploy Namespace=train-ticket
+kubectl wait --for=condition=Ready pod --all --timeout=300s --namespace train-ticket
